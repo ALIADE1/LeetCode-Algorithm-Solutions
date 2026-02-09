2aaaf5174ae3075663e46dc5f875b296d2cfc60a
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2026-02-09 07:06:35 +0200

    Create 1038. Binary Search Tree to Greater Sum Tree.py

---
+++ 1038. Binary Search Tree to Greater Sum Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    sum = 0
+    def bstToGst(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+
+        self.bstToGst(root.right)
+        self.sum+=root.val
+        root.val = self.sum
+        self.bstToGst(root.left)
+
+        return root
