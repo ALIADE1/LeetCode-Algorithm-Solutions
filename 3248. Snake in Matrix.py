8c752ab4ea3dd3167cad515b7dd97a674e6a8246
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2026-02-15 15:30:45 +0200

    Create 3248. Snake in Matrix.py

---
+++ 3248. Snake in Matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        summ_ans = 0
+        i, j = 0, 0
+
+        for com in commands :
+            if com == "RIGHT":
+                j+=1
+                summ_ans=((i * n) + j)
+            elif com == "LEFT":
+                j-=1
+                summ_an+=((i * n) + j)
+            elif com == "UP":
+                i-=1
+                summ_ans=((i * n) + j)
+            else:
+                i+=1
+                summ_ans=((i * n) + j)
+            # print(summ_ans)
+
+        return summ_ans
