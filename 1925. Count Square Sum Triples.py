21019bba4faef9e6356bedc004cba2c2855d24e5
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2026-02-24 13:03:47 +0200

    Create 1925. Count Square Sum Triples.py

---
+++ 1925. Count Square Sum Triples.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        ans_cou = 0
+
+        for a in range(1,n+1):
+            for b in range(a+1,n+1):
+                for c in range(b+1,n+1):
+                    if (a**2 + b**2) == c**2:
+                        ans_cou+=2
+
+        return ans_cou
