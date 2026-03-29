c092247c13023403294d89d4837ece896444f8b0
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2026-03-29 20:39:42 +0200

    Implement findDifference method to compare two arrays

---
+++ 2215. Find the Difference of Two Arrays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        answer1, answer2 = [], []
+
+        for x in nums1:
+            if x not in nums2 and x not in answer1:
+                answer1.append(x)
+            else:
+                continue
+
+        for x in nums2:
+            if x not in nums1 and x not in answer2:
+                answer2.append(x)
+            else:
+                continue
+
+        return [answer1, answer2]
